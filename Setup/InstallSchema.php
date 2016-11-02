f917411960a9506ea1f47771d542925b65bcb260
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2016-11-02 21:28:46 +0100

    Add Setup folder to phpcs and phpunit

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Mondido
+ *
+ * PHP version 5.6
+ *
+ * @category Mondido
+ * @package  Mondido_Mondido
+ * @author   Andreas Karlsson <andreas@kodbruket.se>
+ * @license  MIT License https://opensource.org/licenses/MIT
+ * @link     https://www.mondido.com
+ */
 
 namespace Mondido\Mondido\Setup;
 
@@ -6,8 +17,25 @@ use Magento\Framework\Setup\InstallSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 
+/**
+ * Install schema
+ *
+ * @category Mondido
+ * @package  Mondido_Mondido
+ * @author   Andreas Karlsson <andreas@kodbruket.se>
+ * @license  MIT License https://opensource.org/licenses/MIT
+ * @link     https://www.mondido.com
+ */
 class InstallSchema implements InstallSchemaInterface
 {
+    /**
+     * Install
+     *
+     * @param Magento\Framework\Setup\SchemaSetupInterface   $setup   Schema setup interface
+     * @param Magento\Framework\Setup\ModuleContextInterface $context Module context interface
+     *
+     * @return void
+     */
     public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
         $installer = $setup;
@@ -19,7 +47,7 @@ class InstallSchema implements InstallSchemaInterface
             $installer->getTable('sales_order_grid')
         ];
 
-        foreach($tables as $table) {
+        foreach ($tables as $table) {
             $installer->getConnection()->addColumn(
                 $table,
                 'mondido_transaction',
