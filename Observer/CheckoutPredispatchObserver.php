58268708c3cd4e127a177a357fffe843ca33d94a
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2016-11-03 19:45:47 +0100

    Fix coding standard

--- Observer/CheckoutPredispatchObserver.php
+++ Observer/CheckoutPredispatchObserver.php
@@ -1,17 +1,52 @@
 <?php
+/**
+ * Mondido
+ *
+ * PHP version 5.6
+ *
+ * @category Mondido
+ * @package  Mondido_Mondido
+ * @author   Andreas Karlsson <andreas@kodbruket.se>
+ * @license  MIT License https://opensource.org/licenses/MIT
+ * @link     https://www.mondido.com
+ */
+
 namespace Mondido\Mondido\Observer;
 
 use Magento\Framework\Event\ObserverInterface;
 
+/**
+ * Checkout predispatch observer
+ *
+ * @category Mondido
+ * @package  Mondido_Mondido
+ * @author   Andreas Karlsson <andreas@kodbruket.se>
+ * @license  MIT License https://opensource.org/licenses/MIT
+ * @link     https://www.mondido.com
+ */
 class CheckoutPredispatchObserver implements ObserverInterface
 {
     protected $transaction;
 
+    /**
+     * Constructor
+     *
+     * @param \Mondido\Mondido\Model\Api\Transaction $transaction Transaction API model
+     *
+     * @return void
+     */
     public function __construct(\Mondido\Mondido\Model\Api\Transaction $transaction)
     {
         $this->transaction = $transaction;
     }
 
+    /**
+     * Execute
+     *
+     * @param \Magento\Framework\Event\Observer $observer Observer object
+     *
+     * @return void
+     */
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         $quote = $observer->getEvent()->getControllerAction()->getOnepage()->getQuote();
