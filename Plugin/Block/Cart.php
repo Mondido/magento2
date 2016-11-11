40b925f613595db6c2e1d7ec9f8a3caf12ab0656
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2016-11-11 09:25:52 +0100

    Add plugins to modify getCheckoutUrl methods

---
+++ Plugin/Block/Cart.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Mondido
+ *
+ * PHP version 5.6
+ *
+ * @category Mondido
+ * @package  Mondido_Mondido
+ * @author   Andreas Karlsson <andreas@kodbruket.se>
+ * @license  MIT License https://opensource.org/licenses/MIT
+ * @link     https://www.mondido.com
+ */
+
+namespace Mondido\Mondido\Plugin\Block;
+
+/**
+ * Cart block plugin
+ *
+ * @category Mondido
+ * @package  Mondido_Mondido
+ * @author   Andreas Karlsson <andreas@kodbruket.se>
+ * @license  MIT License https://opensource.org/licenses/MIT
+ * @link     https://www.mondido.com
+ */
+class Cart
+{
+    protected $urlBuilder;
+    protected $config;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\UrlInterface $urlBuilder URL builder
+     * @param \Mondido\Mondido\Model\Config   $config     Config model
+     */
+    public function __construct(UrlInterface $urlBuilder, Config $config)
+    {
+        $this->urlBuilder = $urlBuilder;
+        $this->config = $config;
+    }
+
+    /**
+     * After getCheckoutUrl()
+     *
+     * @param \Magento\Checkout\Block\Cart $subject Checkout cart block
+     * @param string                       $result  The original result from the method in $subject
+     * 
+     * @return string
+     */
+    public function afterGetCheckoutUrl(\Magento\Checkout\Block\Cart $subject, $result)
+    {
+        if (!$this->config->isActive()) {
+            return $result;
+        }
+
+        return $this->urlBuilder->getUrl('mondido/checkout', ['_secure' => true]);
+    }
+}
---
+++ Plugin/Model/DefaultConfigProvider.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Mondido
+ *
+ * PHP version 5.6
+ *
+ * @category Mondido
+ * @package  Mondido_Mondido
+ * @author   Andreas Karlsson <andreas@kodbruket.se>
+ * @license  MIT License https://opensource.org/licenses/MIT
+ * @link     https://www.mondido.com
+ */
+
+namespace Mondido\Mondido\Plugin\Model;
+
+use Magento\Framework\UrlInterface;
+use Mondido\Mondido\Model\Config;
+
+/**
+ * Default config provider model plugin
+ *
+ * @category Mondido
+ * @package  Mondido_Mondido
+ * @author   Andreas Karlsson <andreas@kodbruket.se>
+ * @license  MIT License https://opensource.org/licenses/MIT
+ * @link     https://www.mondido.com
+ */
+class DefaultConfigProvider
+{
+    protected $urlBuilder;
+    protected $config;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\UrlInterface $urlBuilder URL builder
+     * @param \Mondido\Mondido\Model\Config   $config     Config model
+     */
+    public function __construct(UrlInterface $urlBuilder, Config $config)
+    {
+        $this->urlBuilder = $urlBuilder;
+        $this->config = $config;
+    }
+
+    /**
+     * After getCheckoutUrl()
+     *
+     * @param \Magento\Checkout\Model\DefaultConfigProvider $subject Default config provider model
+     * @param string                                        $result  The original result from the method in $subject
+     * 
+     * @return string
+     */
+    public function afterGetCheckoutUrl(\Magento\Checkout\Model\DefaultConfigProvider $subject, $result)
+    {
+        if (!$this->config->isActive()) {
+            return $result;
+        }
+
+        return $this->urlBuilder->getUrl('checkout');
+    } 
+}
