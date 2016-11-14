a781c763c0a9a202fe8929118229d1b43118daec
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2016-11-14 08:50:19 +0100

    Start working with the quote on payment webhook

--- Controller/Payment/Index.php
+++ Controller/Payment/Index.php
@@ -16,6 +16,8 @@ namespace Mondido\Mondido\Controller\Payment;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\Result\JsonFactory;
 use Psr\Log\LoggerInterface;
+use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Quote\Api\CartManagementInterface;
 
 /**
  * Payment action
@@ -39,20 +41,39 @@ class Index extends \Magento\Framework\App\Action\Action
     protected $logger;
 
     /**
+     * @var \Magento\Quote\Api\CartRepositoryInterface
+     */
+    protected $quoteRepository;
+
+    /**
+     * @var \Magento\Quote\Api\CartManagementInterface
+     */
+    protected $quoteManagement;
+
+    /**
      * Constructor
      *
      * @param \Magento\Framework\App\Action\Context            $context           Context object
      * @param \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory Result factory
      * @param \Psr\Log\LoggerInterface                         $logger            Logger interface
+     * @param \Magento\Quote\Api\CartRepositoryInterface       $quoteRepository   Cart repository interface
+     * @param \Magento\Quote\Api\CartManagementInterface       $quoteManagement   Cart management interface
      *
      * @return void
      */
-    public function __construct(Context $context, JsonFactory $resultJsonFactory, LoggerInterface $logger)
-    {
+    public function __construct(
+        Context $context,
+        JsonFactory $resultJsonFactory,
+        LoggerInterface $logger,
+        CartRepositoryInterface $quoteRepository,
+        CartManagementInterface $quoteManagement
+    ) {
         parent::__construct($context);
 
         $this->resultJsonFactory = $resultJsonFactory;
         $this->logger = $logger;
+        $this->quoteRepository = $quoteRepository;
+        $this->quoteManagement = $quoteManagement;
     }
 
     /**
@@ -64,6 +85,12 @@ class Index extends \Magento\Framework\App\Action\Action
     {
         $data = $this->getRequest()->getPostValue();
         $this->logger->debug(var_export($data, true));
+
+        if (array_key_exists('status', $data) && in_array($data['status'], ['approved', 'authorized'])) {
+            $quoteId = $data['payment_ref'];
+            $quote = $this->quoteRepository->get($quoteId);
+            $order = $this->quoteManagement->submit($quote);
+        }
 
         $response = json_encode(['code' => 200]);
 
