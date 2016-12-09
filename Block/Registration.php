6dab773d29401487d1808e517e0dadb74848a149
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2016-12-09 10:58:00 +0100

    Skip registration on success page

---
+++ Block/Registration.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Mondido\Mondido\Block;
+
+class Registration extends \Magento\Checkout\Block\Registration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function toHtml()
+    {
+        return '';
+    }
+}
