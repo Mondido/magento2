a1bb239d56227fe12cff6327ee633331e0421012
Andreas Karlsson <andreas@kodbruket.se>
2017-03-06 09:32:12 +0100

    Add iFrame Resizer to enable dynamic height of iframe Requires iframeResizer.contentWindow.min.js to be implemented in the Mondido template. See http://davidjbradshaw.github.io/iframe-resizer/ for details.

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    map: {
+        '*': {
+            iframeResizer: 'Mondido_Mondido/js/iframeResizer'
+        }
+    },
+    shim: {
+        iframeResizer: ['jquery']
+    }
+};
