3be6762e15ab2c14d0a8965102f6971dc33c8a3b
Robert Lord <robert@codepeak.se>
2017-06-24 09:39:28 +0200

    Add tests for all files

---
+++ Test/Unit/Block/Info/HostedWindowTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Mondido
+ *
+ * PHP version 5.6
+ *
+ * @category Mondido
+ * @package  Mondido_Mondido
+ * @author   Andreas Karlsson <andreas@kodbruket.se>
+ * @license  MIT License https://opensource.org/licenses/MIT
+ * @link     https://www.mondido.com
+ */
+
+namespace Mondido\Mondido\Test\Unit\Block\Info;
+
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager as ObjectManager;
+
+/**
+ * HostedWindowTest
+ *
+ * @category Mondido
+ * @package  Mondido_Mondido
+ * @author   Andreas Karlsson <andreas@kodbruket.se>
+ * @license  MIT License https://opensource.org/licenses/MIT
+ * @link     https://www.mondido.com
+ */
+class HostedWindowTest extends \PHPUnit_Framework_TestCase
+{
+    protected $object;
+
+    /**
+     * @var \Magento\Framework\TestFramework\Unit\Helper\ObjectManager
+     */
+    protected $objectManager;
+
+    /**
+     * Set up
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        $this->objectManager = new ObjectManager($this);
+        $this->object = $this->objectManager->getObject(
+            'Mondido\Mondido\Block\Info\HostedWindow'
+        );
+    }
+
+    /**
+     * Test execute
+     *
+     * @return void
+     */
+    public function testExecute()
+    {
+        $this->assertEquals(get_class($this->object), 'Mondido\Mondido\Block\Info\HostedWindow');
+    }
+
+    /**
+     * Tear down
+     *
+     * @return void
+     */
+    protected function tearDown()
+    {
+        $this->object = null;
+        $this->objectManager = null;
+    }
+}
